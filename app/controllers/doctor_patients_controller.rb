9883e3e9603be53ac6236d5ae3c7d071fb08ad5b
J Seymour <JustJakeSeymour@gmail.com>
2023-01-13 16:36:27 -0700

    fix params id issue for delete doctor patient

--- app/controllers/doctor_patients_controller.rb
+++ app/controllers/doctor_patients_controller.rb
@@ -1,10 +1,7 @@
 class DoctorPatientsController < ApplicationController
   def destroy
-    # id's have been reversed in params using button_to helper... 
-    # I will investigate how to propperly swap these if I have time.
+    DoctorPatient.destroy(params[:id])
     
-    DoctorPatient.destroy(params[:doctor_id])
-    
-    redirect_to doctor_path(params[:id])
+    redirect_to doctor_path(params[:doctor_id])
   end
 end
